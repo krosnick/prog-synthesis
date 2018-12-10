528caee4bc20bfc0bfa3e387c6e84d2ce7d95a76
Rebecca Krosnick <rkros@umich.edu>
2018-12-10 02:22:33 -0500

    Chosen code solution replaces 'output example' in editor; also, stylistic improvements

--- src/app/html/ui.js
+++ src/app/html/ui.js
@@ -1,12 +1,35 @@
 // import * as fs from "fs";
 
+var selectedCode;
+var fullInputCode;
+var leftOfEquals;
 
 $(document).ready(function() {
-    $('#synthesize').on('click', getResults);
+    $('#synthesizeButton').on('click', function(){
+        selectedCode = window.getSelection().toString().trim();
+        fullInputCode = monaco.editor.getModels()[0].getValue().toString().trim();
+        $("#origCode").html(selectedCode);
+
+        getResults();
+    });
     $("#results").on("click", ".btn", function(e){
         $("#results .btn").removeClass("active");
         $(event.target).parent(".btn").addClass("active");
         // Should 
+    });
+
+    $("#replaceButton").on("click", function(){
+        var selectedOutput = $(".active").text();
+        console.log(selectedOutput);
+        replaceCodeSolutionInEditor(selectedOutput);
+
+        // Clear list of solutions
+        $("#list_of_solutions").empty();
+        // Clear orig code
+        $("#origCode").empty();
+
+        // Hide results
+        $("#results").hide();
     });
 });
 
@@ -22,38 +45,50 @@ function getResults() {
 //     // });
 //     // file.open('w')
 //     // file.write(user_input);
-    var selectedCode = window.getSelection().toString().trim();
-    //console.log(selectedCode);
-    //console.log($("#program"));
-    //{"inputCode": , "desiredOutput": selectedCode}
-
-    // input code
-    var fullInputCode = monaco.editor.getModels()[0].getValue().toString().trim();
+    
     //console.log(fullInputCode);
 
     var indexOfSelectedCode = fullInputCode.indexOf(selectedCode);
     var codeBeforeSelection = fullInputCode.substring(0, indexOfSelectedCode);
     //console.log(codeBeforeSelection);
 
-	$.post('/user_input', {desiredOutput: selectedCode, providedInput: codeBeforeSelection}, updateDOM);
+	$.post('/user_input', {desiredOutput: selectedCode, providedInput: codeBeforeSelection}, addCodeSolutionsToDOM);
 	// outputFileSync('exampleInput.ts', user_input);
 }
 
-function updateDOM(results) {
+function putSolutionInVariableStatement(codeSolutionValue){
+    leftOfEquals = selectedCode.substring(0, selectedCode.indexOf("="));
+    var codeSolution = leftOfEquals + "= " + codeSolutionValue + ";";
+    return codeSolution;
+}
+
+function addCodeSolutionsToDOM(results) {
     var codeSolutions = results["codeSolutions"];
     //console.log(codeSolutions);
     //document.getElementById("results").innerHTML = results;
     var btnGroup = $('<div class="btn-group-vertical btn-group-toggle" data-toggle="buttons" ></div>');
     codeSolutions.forEach(function(solution, index){
-        btnGroup.append('<label class="btn btn-light"><input type="radio" name="options" id="solution_' + index + '" autocomplete="off">' + solution + '</label>');
+        btnGroup.append('<label class="btn btn-light btn-sm codeText"><input type="radio" name="options" id="solution_' + index + '" autocomplete="off">' + putSolutionInVariableStatement(solution) + '</label>');
     });
     $("#list_of_solutions").append(btnGroup);
     $("#results").show();
 }
 // Callback function that updates the DOM once the post data (programSynthesis results) comes back
 
+function replaceCodeSolutionInEditor(codeSolution){
+    var indexOfSelectedCode = fullInputCode.indexOf(selectedCode);
+    var codeBeforeSelection = fullInputCode.substring(0, indexOfSelectedCode);
+    var codeAfterSelection = fullInputCode.substring(indexOfSelectedCode + selectedCode.length);
+    
+    var newFullEditorCode = codeBeforeSelection + codeSolution + codeAfterSelection;
+    //console.log(newFullEditorCode);
+    monaco.editor.getModels()[0].setValue(newFullEditorCode);
+}
+
+
 function runProgram() {
     programOutput = eval(monaco.editor.getModels()[0].getValue().toString());
+    
     // alert(programOutput);
     // Get string of the code in the input file
     // let inputFileContentsString = fs.readFileSync(fileNameRequiredInput, "utf8");
